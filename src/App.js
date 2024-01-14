10360bd068aa4772d92eea11e89ad778e43e9c6e
amantale <amantale@bandwidth.com>
2024-01-14 20:27:14 +0200

    implemented 2 routes for fetch all notes and create a new note

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 // src/App.js
 
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';  // Import Routes
 import NotesList from './components/NotesList';
+import CreateNote from './components/CreateNote';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <NotesList />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <h1>Notes app</h1>
+            <ul>
+              <li>
+                <Link to="/see-all-notes">See All of My Notes</Link>
+              </li>
+              <li>
+                <Link to="/create-note">Create a Note</Link>
+              </li>
+            </ul>
+          </nav>
+          
+          <Routes>  {}
+            <Route path="/see-all-notes" element={<NotesList />} />
+            <Route path="/create-note" element={<CreateNote />} />
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/CreateNote.js
@@ -0,0 +1,14 @@
+// src/components/CreateNote.js
+
+import React from 'react';
+
+const CreateNote = () => {
+  return (
+    <div>
+      <h1>Create a Note</h1>
+      <p>Hello!</p>
+    </div>
+  );
+};
+
+export default CreateNote;
